236282b4bfa4c3373b24332eac9673dcf40c9fad
KsuVla <ksu.xa.v.ya@gmail.com>
2021-08-29 02:34:21 +0700

    created project and added 'start-page' with 'link', 'fonts'

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './pages/start-page/start-page.scss';
+import Link from './blocks/link/link';
+
+function App() {
+  return (
+    <div class="start-page">
+      <h2>Pages Toxin:</h2>
+      <div class="start-page__links">
+        <Link classBlock="start-page__link" text="landing page" url="./landing-page.html" />
+        <Link classBlock="start-page__link" text="registration" />
+        <Link classBlock="start-page__link" text="sing in" />
+        <Link classBlock="start-page__link" text="search room" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/blocks/link/link.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './link.scss';
+
+class Link extends React.Component {
+  constructor(props) {
+    super(props);
+    this.date = {
+      url: './link-stub',
+      text: 'Ссылка',
+      type: '',
+      classBlock: '',
+    };
+  }
+  defineUrl() {
+    return this.props.url || this.date.url;
+  }
+  defineText() {
+    return this.props.text || this.date.text;
+  }
+  defineType() {
+    const type = this.props.type || this.date.type;
+    switch (type) {
+      case 'gray': {
+        return 'link link_gray';
+      }
+      case 'clean': {
+        return 'link link_clean';
+      }
+      case 'ok': {
+        return 'link link_ok';
+      }
+      default: {
+        return 'link';
+      }
+    }
+  }
+  render() {
+    return (
+      <div class={this.props.classBlock}>
+        <a class={this.defineType()} href={this.defineUrl()}>
+          {this.defineText()}
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Link;
