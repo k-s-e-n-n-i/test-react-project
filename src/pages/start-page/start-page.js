013fc3e2b8f2b032e8b6b10f31fad41139b0d824
KsuVla <ksu.xa.v.ya@gmail.com>
2022-01-13 18:42:30 +0700

    fixed 'StartPage'

--- src/pages/start-page/start-page.js
+++ src/pages/start-page/start-page.js
@@ -1,18 +1,27 @@
+import React from 'react';
 import './start-page.scss';
-import Link from './blocks/link/link';
+import Link from '../../components/link/link';
 
-function StartPage() {
-  return (
-    <div class="start-page">
-      <h2>Pages Toxin:</h2>
-      <div class="start-page__links">
-        <Link classBlock="start-page__link" text="landing page" url="./landing-page.html" />
-        <Link classBlock="start-page__link" text="registration" />
-        <Link classBlock="start-page__link" text="sing in" />
-        <Link classBlock="start-page__link" text="search room" />
+class StartPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.data = {};
+  }
+  render() {
+    return (
+      <div>
+        <div className="start-page">
+          <h2>Pages Toxin:</h2>
+          <div className="start-page__links">
+            <Link classBlock="start-page__link" text="landing page" url="./landing-page.html" />
+            <Link classBlock="start-page__link" text="registration" />
+            <Link classBlock="start-page__link" text="sing in" />
+            <Link classBlock="start-page__link" text="search room" />
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default StartPage;
