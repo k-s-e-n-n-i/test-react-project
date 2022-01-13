31972b110aed7985e9da1cb4954a62a3cc5d318d
KsuVla <ksu.xa.v.ya@gmail.com>
2022-01-13 17:35:38 +0700

    added component "Social'

---
+++ src/components/social/social.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './social.scss';
+
+class Social extends React.Component {
+  constructor(props) {
+    super(props);
+    this.twitter = {
+      on: this.props.twitter || false,
+      name: 'twitter',
+      link: this.props.twitter || './link-stub',
+      icon: require('./img/twitter.svg').default,
+    };
+    this.facebook = {
+      on: this.props.facebook || false,
+      name: 'facebook',
+      link: this.props.facebook || './link-stub',
+      icon: require('./img/facebook.svg').default,
+    };
+    this.instagram = {
+      on: this.props.instagram || false,
+      name: 'instagram',
+      link: this.props.instagram || './link-stub',
+      icon: require('./img/instagram.svg').default,
+    };
+
+    this.socs = [this.twitter, this.facebook, this.instagram];
+  }
+
+  render() {
+    return (
+      <div className="social">
+        {this.socs.map(
+          (soc, index) =>
+            soc.on && (
+              <span className="social__item">
+                <a href={soc.link}>
+                  <img className={`social__item ${soc.name}`} src={soc.icon} alt={soc.name}></img>
+                </a>
+              </span>
+            )
+        )}
+      </div>
+    );
+  }
+}
+
+export default Social;
