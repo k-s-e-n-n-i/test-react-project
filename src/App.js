ee6edca6d2155cc051a8579f9e63f530ff4f8014
KsuVla <ksu.xa.v.ya@gmail.com>
2022-01-14 18:32:57 +0700

    fixed build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './pages/start-page/start-page.scss';
-import Layout from './components/layout/layout';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { basename } from './modules/functions/functions';
+
+import './pages/start-page/start-page.scss';
+
+import Layout from './components/layout/layout';
 import StartPage from './pages/start-page/start-page';
 import LandingPage from './pages/landing-page/landing-page';
 
-function App() {
-  const startPage = () => <StartPage />;
-  const landingPage = () => <LandingPage />;
-  return (
-    <Router>
-      <Route exact path="/">
-        <Layout header={0} content={startPage} footer={0} />
-      </Route>
-      <Route exact path="/landing-page.html">
-        <Layout content={landingPage} />
-      </Route>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.data = {};
+  }
+  render() {
+    const startPage = () => <StartPage />;
+    const landingPage = () => <LandingPage />;
+
+    return (
+      <Router basename={basename}>
+        <Switch>
+          <Route exact path="/">
+            <Layout header={0} content={startPage} footer={0} />
+          </Route>
+          <Route exact path="/landing-page.html">
+            <Layout content={landingPage} />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/pages/start-page/start-page.js
+++ src/pages/start-page/start-page.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './start-page.scss';
 import Link from '../../components/link/link';
 
+import { basename } from '../../modules/functions/functions';
+
 class StartPage extends React.Component {
   constructor(props) {
     super(props);
@@ -13,7 +15,7 @@ class StartPage extends React.Component {
         <div className="start-page">
           <h2>Pages Toxin:</h2>
           <div className="start-page__links">
-            <Link classBlock="start-page__link" text="landing page" url="./landing-page.html" />
+            <Link classBlock="start-page__link" text="landing page" url={`${basename}/landing-page.html`} />
             <Link classBlock="start-page__link" text="registration" />
             <Link classBlock="start-page__link" text="sing in" />
             <Link classBlock="start-page__link" text="search room" />
